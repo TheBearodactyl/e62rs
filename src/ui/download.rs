730b9c9a5f28406c8bb01c42be3231ec4cd68eab
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-09-19 20:10:01 -0500

    way more formatting params

--- src/ui/download.rs
+++ src/ui/download.rs
@@ -69,26 +69,97 @@ impl PostDownloader {
             .map(|s| s.as_str())
             .unwrap_or("unknown");
 
-        let formatted = format
-            .replace("$rating", &post.rating)
-            .replace("$artist", artist)
+        let re = regex::Regex::new(r"\$tags\[(\d+)\]").unwrap();
+        let mut formatted = format.to_string();
+
+        for cap in re.captures_iter(format) {
+            if let Some(num_match) = cap.get(1) {
+                if let Ok(num_tags) = num_match.as_str().parse::<usize>() {
+                    let tags = post
+                        .tags
+                        .general
+                        .iter()
+                        .take(num_tags)
+                        .cloned()
+                        .collect::<Vec<String>>()
+                        .join(",");
+
+                    formatted = formatted.replace(&cap[0], &tags);
+                }
+            }
+        }
+
+        let now = chrono::Local::now();
+        let rating_first = post
+            .rating
+            .chars()
+            .next()
+            .unwrap_or('u')
+            .to_lowercase()
+            .to_string();
+
+        let (year, month, day, hour, minute, second) =
+            if let Ok(created_date) = chrono::DateTime::parse_from_rfc3339(&post.created_at) {
+                (
+                    created_date.format("%Y").to_string(),
+                    created_date.format("%m").to_string(),
+                    created_date.format("%d").to_string(),
+                    created_date.format("%H").to_string(),
+                    created_date.format("%M").to_string(),
+                    created_date.format("%S").to_string(),
+                )
+            } else {
+                (
+                    now.format("%Y").to_string(),
+                    now.format("%m").to_string(),
+                    now.format("%d").to_string(),
+                    now.format("%H").to_string(),
+                    now.format("%M").to_string(),
+                    now.format("%S").to_string(),
+                )
+            };
+
+        let formatted = formatted
             .replace("$id", &post.id.to_string())
+            .replace("$rating", &post.rating)
+            .replace("$rating_first", &rating_first)
             .replace("$score", &post.score.total.to_string())
-            .replace("$score_up", &post.score.up.to_string())
-            .replace("$score_down", &post.score.down.to_string())
+            .replace("$fav_count", &post.fav_count.to_string())
+            .replace("$comment_count", &post.comment_count.to_string())
             .replace("$md5", &post.file.md5)
-            .replace("$filesize", &post.file.size.to_string())
-            .replace("$ext", &post.file.ext);
+            .replace("$ext", &post.file.ext)
+            .replace("$width", &post.file.width.to_string())
+            .replace("$height", &post.file.height.to_string())
+            .replace("$size", &post.file.size.to_string())
+            .replace("$artist", artist)
+            .replace("$uploader", &post.uploader_name)
+            .replace("$uploader_id", &post.uploader_id.to_string())
+            .replace("$year", &year)
+            .replace("$month", &month)
+            .replace("$day", &day)
+            .replace("$hour", &hour)
+            .replace("$minute", &minute)
+            .replace("$second", &second)
+            .replace("$date", &format!("{}-{}-{}", year, month, day))
+            .replace("$time", &format!("{}-{}-{}", hour, minute, second))
+            .replace(
+                "$datetime",
+                &format!("{}-{}-{} {}-{}-{}", year, month, day, hour, minute, second),
+            )
+            .replace("$now_year", &now.format("%Y").to_string())
+            .replace("$now_month", &now.format("%m").to_string())
+            .replace("$now_day", &now.format("%d").to_string())
+            .replace("$now_hour", &now.format("%H").to_string())
+            .replace("$now_minute", &now.format("%M").to_string())
+            .replace("$now_second", &now.format("%S").to_string())
+            .replace("$now_date", &now.format("%Y-%m-%d").to_string())
+            .replace("$now_time", &now.format("%H-%M-%S").to_string())
+            .replace(
+                "$now_datetime",
+                &now.format("%Y-%m-%d %H-%M-%S").to_string(),
+            );
 
         Ok(formatted)
-    }
-
-    fn extract_filename(&self, url: &str, post: &E6Post) -> Result<String> {
-        url.split('/')
-            .next_back()
-            .map(String::from)
-            .or_else(|| Some(format!("{}.{}", post.id, post.file.ext)))
-            .context("Failed to extract filename from URL")
     }
 
     fn get_filepath(&self, filename: &str) -> Result<PathBuf> {
