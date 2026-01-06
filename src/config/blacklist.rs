90ef93128257394fdd716c66fd3e7f89079276d3
The Bearodactyl <dabewodactowluwu@gmail.com>
2026-01-06 15:22:30 -0600

    improvement: clean up code in `blacklist.rs`

--- src/config/blacklist.rs
+++ src/config/blacklist.rs
@@ -8,15 +8,14 @@ use {
 pub fn add_to_blacklist(tag: String) -> Result<()> {
     let mut cfg = config_mut().wrap_err("failed to get write lock for config")?;
 
-    if let Some(ref mut search_cfg) = cfg.search {
-        if let Some(blacklist) = search_cfg.blacklist.as_mut() {
-            if !blacklist.contains(&tag) {
-                blacklist.push(tag);
-                blacklist.sort();
+    if let Some(ref mut search_cfg) = cfg.search
+        && let Some(blacklist) = search_cfg.blacklist.as_mut()
+        && !blacklist.contains(&tag)
+    {
+        blacklist.push(tag);
+        blacklist.sort();
 
-                cfg.save().wrap_err("failed to save config");
-            }
-        }
+        cfg.save().wrap_err("failed to save config")?;
     }
     Ok(())
 }
@@ -25,15 +24,14 @@ pub fn add_to_blacklist(tag: String) -> Result<()> {
 pub fn remove_from_blacklist(tag: &str) -> Result<bool> {
     let mut cfg = config_mut().wrap_err("failed to get write lock for config")?;
 
-    if let Some(ref mut search_cfg) = cfg.search {
-        if let Some(blacklist) = search_cfg.blacklist.as_mut() {
-            if let Some(pos) = blacklist.iter().position(|x| x == tag) {
-                blacklist.remove(pos);
-                cfg.save().wrap_err("failed to save config")?;
+    if let Some(ref mut search_cfg) = cfg.search
+        && let Some(blacklist) = search_cfg.blacklist.as_mut()
+        && let Some(pos) = blacklist.iter().position(|x| x == tag)
+    {
+        blacklist.remove(pos);
+        cfg.save().wrap_err("failed to save config")?;
 
-                return Ok(true);
-            }
-        }
+        return Ok(true);
     }
 
     Ok(false)
@@ -43,11 +41,11 @@ pub fn remove_from_blacklist(tag: &str) -> Result<bool> {
 pub fn clear_blacklist() -> Result<()> {
     let mut cfg = config_mut().wrap_err("failed to get write lock for config")?;
 
-    if let Some(ref mut search_cfg) = cfg.search {
-        if let Some(blacklist) = search_cfg.blacklist.as_mut() {
-            blacklist.clear();
-            cfg.save().wrap_err("failed to save config")?;
-        }
+    if let Some(ref mut search_cfg) = cfg.search
+        && let Some(blacklist) = search_cfg.blacklist.as_mut()
+    {
+        blacklist.clear();
+        cfg.save().wrap_err("failed to save config")?;
     }
 
     Ok(())
@@ -56,13 +54,13 @@ pub fn clear_blacklist() -> Result<()> {
 /// get a copy of the current blacklist
 pub fn get_blacklist() -> Result<Vec<String>> {
     let cfg = config().wrap_err("failed to get read lock for config")?;
-    if let Some(ref search_cfg) = cfg.search {
-        if let Some(blacklist) = search_cfg.blacklist.clone() {
-            return Ok(blacklist);
-        }
+    if let Some(ref search_cfg) = cfg.search
+        && let Some(blacklist) = search_cfg.blacklist.clone()
+    {
+        return Ok(blacklist);
     }
 
-    return Ok(Vec::new());
+    Ok(Vec::new())
 }
 
 /// check if a tag is blacklisted
